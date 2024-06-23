92135c0b7bb4e88b1d3be93702e5b531886bf86c
RychidM <Richardsk2@gmail.com>
2024-06-23 21:28:38 +0000

    feat: Add new regex patterns for phone number validation and required text

--- lib/rm_strings.dart
+++ lib/rm_strings.dart
@@ -2,4 +2,6 @@ class RmStrings {
   RmStrings._();
 
   static const String rPhoneNumberRegex = r"^(\d{1,3}[-]?)?\d{10}$";
+  static const String rPhoneNumberWithoutCtryCodeRegex = r"^\d{7,10}$";
+  static const String rRequiredText = "Required";
 }

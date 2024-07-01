1c88f8f1fe5bea2ea17ae82c7c481fafc237d318
RychidM <Richardsk2@gmail.com>
2024-07-01 22:26:26 +0000

    added state status for bloc

--- lib/state_status.dart
+++ lib/state_status.dart
@@ -1,7 +1,15 @@
-abstract class StateStatus {
+sealed class StateStatus {
   const StateStatus();
 }
 
 class InitialFormStatus extends StateStatus {
   const InitialFormStatus();
 }
+
+class ErrorStatusWithException extends StateStatus {
+  final Exception e;
+
+  const ErrorStatusWithException({required this.e});
+}
+
+class ErrorOccurredStatus extends StateStatus {}
